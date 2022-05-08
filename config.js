1137ac8553d54e1a89e162b92eafda2916dd174f
KrishnaKEA <82447207+KrishnaKEA@users.noreply.github.com>
2022-05-08 08:44:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	port: process.env.PORT || 3000,
 	mongoURI:
-		"mongodb+srv://krishnaamen:Anita.sanima123@cluster0.7w1bz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+		"mongodb+srv://krishnaamen:password@cluster0.7w1bz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
